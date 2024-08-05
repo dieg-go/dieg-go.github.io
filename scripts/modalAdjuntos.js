e8bafb17bdcec8452f6d393e16c86ebf973384b0
dieg-go <diego.m.arancibia@gmail.com>
2024-08-05 16:49:30 -0400

    prev archivos en ventana popup

--- scripts/modalAdjuntos.js
+++ scripts/modalAdjuntos.js
@@ -75,80 +75,112 @@ function mostrarAdjuntos() {
     modal.show();
 }
 
+function openPreviewPopup(content, title) {
+  const popupWindow = window.open('', '_blank', 'width=800,height=600,resizable=yes,scrollbars=yes');
+  popupWindow.document.write(`
+      <html>
+          <head>
+              <title>${title}</title>
+              <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback" />
+              <link
+                  rel="stylesheet"
+                  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+                  integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+                  crossorigin="anonymous"
+                  referrerpolicy="no-referrer"
+              />
+              <link
+                  rel="stylesheet"
+                  href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.2.0/css/adminlte.min.css"
+                  integrity="sha512-IuO+tczf4J43RzbCMEFggCWW5JuX78IrCJRFFBoQEXNvGI6gkUw4OjuwMidiS4Lm9Q2lILzpJwZuMWuSEeT9UQ=="
+                  crossorigin="anonymous"
+                  referrerpolicy="no-referrer"
+              />
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.0/codemirror.min.css">
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.0/theme/monokai.min.css">
+              <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+              <script
+                  src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+                  integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
+                  crossorigin="anonymous"
+              ></script>
+              <script
+                  src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.2.0/js/adminlte.min.js"
+                  integrity="sha512-KBeR1NhClUySj9xBB0+KRqYLPkM6VvXiiWaSz/8LCQNdRpUm38SWUrj0ccNDNSkwCD9qPA4KobLliG26yPppJA=="
+                  crossorigin="anonymous"
+                  referrerpolicy="no-referrer"
+              ></script>              
+              <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.9.359/pdf.min.js"></script>
+              <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.0/codemirror.min.js"></script>
+              <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.0/mode/xml/xml.min.js"></script>
+          </head>
+          <body>
+              <div class="wrapper" id="previewContainer">
+                  <div class="card" id="previewBody"> </div>
+              </div>
+              <script>
+                  ${content}
+              </script>
+          </body>
+      </html>
+  `);
+  popupWindow.document.close();
+}
+
 function previsualizarAdjunto(nombreArchivo, tituloArchivo) {
-    const extension = nombreArchivo.split(".").pop().toLowerCase();
-    const previewContainer = document.getElementById("previewContainer");
+  const extension = nombreArchivo.split(".").pop().toLowerCase();
+  let content = '';
 
-    previewContainer.innerHTML = `
-    <div class="my-3 card card-outline card-primary">
-      <div class="card-header">
-        <h3 class="card-title">Vista Previa: ${tituloArchivo}</h3>
-        <div class="card-tools">
-          <button type="button" class="btn btn-tool" data-card-widget="collapse">
-            <i class="fas fa-minus"></i>
-          </button>
-          <button type="button" class="btn btn-tool" data-card-widget="remove">
-            <i class="fas fa-times"></i>
-          </button>
-        </div>
-      </div>
-        <div class="card-body" id="previewBody" style="max-height: 500px; overflow-y: auto;">
-      </div>
-    </div>
-  `;
+  if (extension === "pdf") {
+      content = `
+          const url = '/adjuntos/${nombreArchivo}';
+          pdfjsLib.getDocument(url).promise.then((pdf) => {
+              pdf.getPage(1).then((page) => {
+                  const scale = 1.5;
+                  const viewport = page.getViewport({ scale });
+                  const canvas = document.createElement("canvas");
+                  const context = canvas.getContext("2d");
+                  canvas.height = viewport.height;
+                  canvas.width = viewport.width;
+                  page.render({
+                      canvasContext: context,
+                      viewport: viewport,
+                  });
+                  document.getElementById("previewBody").appendChild(canvas);
+              });
+          });
+      `;
+  } else if (extension === "xml") {
+      content = `
+          fetch('/adjuntos/${nombreArchivo}')
+              .then((response) => response.text())
+              .then((data) => {
+                  const editor = CodeMirror(document.getElementById("previewBody"), {
+                      value: data,
+                      mode: "xml",
+                      lineNumbers: true,
+                      readOnly: true,
+                      theme: "monokai",
+                  });
+                  editor.setSize("100%", "100%");
+              });
+      `;
+  } else if (extension === "jpg" || extension === "jpeg" || extension === "png") {
+      content = `
+          const img = document.createElement("img");
+          img.style.maxWidth = "100%";
+          img.src = '/adjuntos/${nombreArchivo}';
+          document.getElementById("previewBody").appendChild(img);
+      `;
+  } else {
+      content = `
+          document.getElementById("previewBody").innerHTML = '
+              <div class="alert alert-info">
+                  <i class="fas fa-info-circle"></i> No se pudo previsualizar el archivo.
+              </div>
+          ';
+      `;
+  }
 
-    if (extension === "pdf") {
-        const url = `/adjuntos/${nombreArchivo}`;
-        pdfjsLib.getDocument(url).promise.then((pdf) => {
-            pdf.getPage(1).then((page) => {
-                const scale = 1.5;
-                const viewport = page.getViewport({ scale });
-                const canvas = document.createElement("canvas");
-                const context = canvas.getContext("2d");
-                canvas.height = viewport.height;
-                canvas.width = viewport.width;
-
-                page.render({
-                    canvasContext: context,
-                    viewport: viewport,
-                });
-
-                document.getElementById("previewBody").appendChild(canvas);
-            });
-        });
-
-        return;
-    }
-
-    if (extension === "xml") {
-        fetch(`/adjuntos/${nombreArchivo}`)
-            .then((response) => response.text())
-            .then((data) => {
-                const editor = CodeMirror(document.getElementById("previewBody"), {
-                    value: data,
-                    mode: "xml",
-                    lineNumbers: true,
-                    readOnly: true,
-                    theme: "monokai",
-                });
-                editor.setSize(null, 400); // Set height to 400px
-            });
-
-        return;
-    }
-
-    if(extension === "jpg" || extension === "jpeg" || extension === "png"){
-        const url = `/adjuntos/${nombreArchivo}`;
-        const img = document.createElement("img");
-        img.src = url;
-        document.getElementById("previewBody").appendChild(img);
-        return;
-
-    }
-
-    document.getElementById("previewBody").innerHTML = `
-                  <div class="alert alert-info">
-                    <i class="fas fa-info-circle"></i> No se pudo previsualizar el archivo.
-                  </div>
-                `;
+  openPreviewPopup(content, `Vista Previa: ${tituloArchivo}`);
 }
